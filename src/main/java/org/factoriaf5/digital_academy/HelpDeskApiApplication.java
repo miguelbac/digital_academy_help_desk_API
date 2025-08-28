16dae9254816eceb5058e8230d291ee51a5d2286
miguelbac <mbacebal@gmail.com>
2025-08-28 09:37:35 +0200

    Init

---
+++ src/main/java/org/factoriaf5/digital_academy/HelpDeskApiApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.digital_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/digital_academy/HelpDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.digital_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
