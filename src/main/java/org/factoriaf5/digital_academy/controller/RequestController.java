e61e68f5378a3ca8c69b23338645b6772b12d29e
Miguel Ballesteros <mbacebal@gmail.com>
2025-09-05 09:55:56 +0200

    chore : small code reformating

--- src/main/java/org/factoriaf5/digital_academy/controller/RequestController.java
+++ src/main/java/org/factoriaf5/digital_academy/controller/RequestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/requests")
+@RequestMapping("/api/v1/requests")
 public class RequestController {
 
     private final RequestService service;
